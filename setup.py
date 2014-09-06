7d89bef9422d400524757d060821793d97678455
Tom Stokes <tomstokes@gmail.com>
2014-09-06 17:55:37 -0600

    Basic setup.py
    Structured as an individual module for now.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="spi",
+      version="0.1.0",
+      description="SPI Interface",
+      long_description="Pure Python SPI interface using spidev",
+      author="Tom Stokes",
+      author_email="tomstokes@gmail.com",
+      url="http://github.com/tomstokes/python-spi",
+      license="MIT",
+      py_modules=['spi'])
